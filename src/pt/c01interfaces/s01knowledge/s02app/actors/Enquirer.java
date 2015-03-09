c3a93641e9409f1ae942e7e506058964c562ed6e
rafaelgpimenta <rafaelgpimenta@gmail.com>
2015-03-09 14:06:41 -0300

    Trabalho

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -7,38 +7,63 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Enquirer implements IEnquirer
 {
-    IObjetoConhecimento obj;
-	
+	IObjetoConhecimento obj;
+
 	public Enquirer()
 	{
 	}
-	
-	
+
+
 	@Override
 	public void connect(IResponder responder)
 	{
-        IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
+		List<String> armazenaPergunta = new ArrayList<String>();
+		List<String> armazenaResposta = new ArrayList<String>();
+		IBaseConhecimento bc = new BaseConhecimento();
+		String[] bichos = bc.listaNomes();
+		int i = 0;
+		IDeclaracao decl = null;
+		do{
 
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
+			obj = bc.recuperaObjeto(bichos[i]);
+			decl = obj.primeira();
+
+			boolean animalEsperado = true;
+			while (decl != null && animalEsperado) {
+				String pergunta = decl.getPropriedade();
+				String respostaEsperada = decl.getValor();
+
+				if(!armazenaPergunta.contains(pergunta)){
+					String resposta = responder.ask(pergunta);
+					armazenaPergunta.add(pergunta);
+					armazenaResposta.add(resposta);
+
+					if (resposta.equalsIgnoreCase(respostaEsperada))
+						decl = obj.proxima();
+					else
+						animalEsperado = false;
+				}
+				else{
+					int indexResposta;
+					indexResposta = armazenaPergunta.indexOf(pergunta);
+					if (armazenaResposta.get(indexResposta).equalsIgnoreCase(respostaEsperada))
+						decl = obj.proxima();
+					else
+						animalEsperado = false;
+				}
+			}
+
+
+			i++;
+		}while(i < bichos.length && decl != null);
+		boolean acertei = responder.finalAnswer(bichos[i]);
+
 		if (acertei)
 			System.out.println("Oba! Acertei!");
 		else
