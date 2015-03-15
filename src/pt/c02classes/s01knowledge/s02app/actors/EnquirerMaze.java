ccdc2980bfe2bbda4ecde30eeaf2f57e0942611e
rafaelgpimenta <rafaelgpimenta@gmail.com>
2015-03-15 18:56:16 -0300

    Trabalho maze funcionando

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -2,8 +2,6 @@ package pt.c02classes.s01knowledge.s02app.actors;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.Stack;
 
 import pt.c02classes.s01knowledge.s01base.inter.IEnquirer;
 import pt.c02classes.s01knowledge.s01base.inter.IResponder;
@@ -11,51 +9,86 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 public class EnquirerMaze implements IEnquirer {
 
 	IResponder responder;
-	
+
 	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
-	
-	public boolean discover() {
-		Stack<String> possibilidades = new Stack<String>();
-		Stack<String> movExecutado = new Stack<String>();
-		List<Integer[][]> posicoesPassdas = new ArrayList<Integer[][]>();
 
-		String posicao = responder.ask("norte");	
-		String movimento;
-		String verifica = "inicio";
-				
-		while(!posicao.equals("saida")){
-			
-			if((responder.ask("leste").equals("passagem") ||responder.ask("leste").equals("saida"))
-					&& !verifica.equals("leste"))
-				possibilidades.push("leste");			
-			if((responder.ask("oeste").equals("passagem") || responder.ask("oeste").equals("saida"))
-					&& !verifica.equals("oeste"))
-				possibilidades.push("oeste");
-			if((responder.ask("sul").equals("passagem") ||responder.ask("sul").equals("saida"))
-					&& !verifica.equals("sul"))
-				possibilidades.push("sul");			
-			if((responder.ask("norte").equals("passagem") ||responder.ask("norte").equals("saida"))
-					&& !verifica.equals("norte"))
-				possibilidades.push("norte");
-			
-			movimento = possibilidades.pop();
-			responder.move(movimento);
-			
-			switch (movimento) {
-				case "norte": movExecutado.push("sul"); break;
-				case "sul":   movExecutado.push("norte"); break;
-				case "leste": movExecutado.push("oeste"); break;
-				case "oeste": movExecutado.push("leste"); break;
-			}
-			
-			posicao = responder.ask("aqui");
-			verifica = movExecutado.peek();
+	public void volta(int linha, int coluna,int linhaAnterior,int colunaAnterior){
+		if(linha == linhaAnterior && coluna > colunaAnterior)
+			responder.move("oeste");
+		else if(linha == linhaAnterior && coluna < colunaAnterior)
+			responder.move("leste");
+		else if(linha > linhaAnterior)
+			responder.move("norte");
+		else
+			responder.move("sul");
+	}
+	public boolean recursao(int linha, int coluna, List<String> posicoesPassadas, boolean resposta,
+			int linhaAnterior,int colunaAnterior) {
+
+		String posicao = responder.ask("aqui");
+		String aux;
+		int direcao;
+		boolean entrou = false;
+		
+		if (posicao.equals("saida")){
+			resposta = true;
+			return resposta;
 		}
 		
+		direcao = coluna + 1;
+		aux = responder.ask("leste");
 		
-		return true;
+		if ((aux.equals("passagem") || aux.equals("saida"))
+				&& !posicoesPassadas.contains(String.valueOf(linha) + "," + String.valueOf(direcao))
+				&& !resposta){
+			posicoesPassadas.add(String.valueOf(linha) + "," + String.valueOf(direcao));
+			responder.move("leste");			
+			resposta = recursao(linha,direcao,posicoesPassadas,resposta,linha,coluna);
+			entrou = resposta;
+		}
+		direcao = coluna - 1;
+		aux = responder.ask("oeste");
+		if ((aux.equals("passagem") || aux.equals("saida"))
+				&& !posicoesPassadas.contains(String.valueOf(linha) + "," + String.valueOf(direcao))
+				&& !resposta){
+			posicoesPassadas.add(String.valueOf(linha) + "," + String.valueOf(direcao));
+			responder.move("oeste");
+			resposta = recursao(linha,direcao,posicoesPassadas,resposta,linha,coluna);
+			entrou = resposta;
+		}
+		direcao = linha + 1;
+		if ((responder.ask("sul").equals("passagem") || responder.ask("sul").equals("saida")) 
+				&& !posicoesPassadas.contains(String.valueOf(direcao) + "," + String.valueOf(coluna))
+				&& !resposta){
+			posicoesPassadas.add(String.valueOf(direcao) + "," + String.valueOf(coluna));
+			responder.move("sul");			
+			resposta = recursao(direcao,coluna,posicoesPassadas,resposta,linha,coluna);
+			entrou = resposta;
+		}
+		direcao = linha - 1;
+		if ((responder.ask("norte").equals("passagem") || responder.ask("norte").equals("saida"))
+				&& !posicoesPassadas.contains(String.valueOf(direcao) + "," + String.valueOf(coluna))
+				&& !resposta){
+			posicoesPassadas.add(String.valueOf(direcao) + "," + String.valueOf(coluna));
+			responder.move("norte");			
+			resposta = recursao(direcao,coluna,posicoesPassadas,resposta,linha,coluna);
+			entrou = resposta;
+		}
+		
+		if(!entrou){
+			volta(linha,coluna, linhaAnterior, colunaAnterior);
+		}
+		
+		return resposta;
 	}
-	
+
+	public boolean discover() {
+		List<String> posicoesPassadas = new ArrayList<String>();
+		posicoesPassadas.add("0,0");		
+
+		return recursao(0,0,posicoesPassadas,false,0,0);
+	}
+
 }
